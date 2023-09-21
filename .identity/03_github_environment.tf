1d66f1b609949e72f589f20aca9f37e618008b1f
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-09-21 16:34:11 +0200

    fix

--- .identity/03_github_environment.tf
+++ .identity/03_github_environment.tf
@@ -37,6 +37,7 @@ locals {
     "SONAR_TOKEN" : data.azurerm_key_vault_secret.key_vault_sonar.value,
     "BOT_TOKEN_GITHUB" : data.azurerm_key_vault_secret.key_vault_bot_token.value,
     "CUCUMBER_PUBLISH_TOKEN" : data.azurerm_key_vault_secret.key_vault_cucumber_token.value,
+    "SLACK_WEBHOOK_URL": data.azurerm_key_vault_secret.key_vault_slack_webhook_url.value
   }
 }
 
@@ -77,3 +78,23 @@ resource "github_actions_secret" "repo_secrets" {
   plaintext_value = each.value
 }
 
+############
+## Labels ##
+############
+resource "github_issue_label" "breaking_change" {
+  repository = local.github.repository
+  name       = "breaking-change"
+  color      = "FF0000"
+}
+
+resource "github_issue_label" "new_release" {
+  repository = local.github.repository
+  name       = "new-release"
+  color      = "FFFF00"
+}
+
+resource "github_issue_label" "ignore_for_release" {
+  repository = local.github.repository
+  name       = "ignore-for-release"
+  color      = "008000"
+}
