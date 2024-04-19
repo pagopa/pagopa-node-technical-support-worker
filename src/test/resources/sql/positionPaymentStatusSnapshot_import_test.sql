afe9bc07385cb8bf0247a563ab74ef689a9fec9f
Lorenzo Catalano <lorenzo.catalano@tasgroup.eu>
2024-04-19 09:38:01 +0200

    fix APP_NAME node-technicalsupport

--- src/test/resources/sql/positionPaymentStatusSnapshot_import_test.sql
+++ src/test/resources/sql/positionPaymentStatusSnapshot_import_test.sql
@@ -1,30 +1,30 @@
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(1860007, '12345678900', '002596607568477200', '596607568477200', '664f1271b06f48878c31a012e74b57de', 'PAID_NORPT', '2023-01-30 23:55:30.009', '2023-01-30 23:55:30.155', 1840007, 'activatePaymentNotice', 'sendPaymentOutcomeV2');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(2780003, '12345678900', '302119397716905800', '02119397716905800', 'e628ae011eeb48e78ee8d32cd25d99bc', 'CANCELLED', '2023-01-31 07:42:38.179', '2023-01-31 08:02:29.031', 2760003, 'activateIOPayment', 'annullamentoRptMaiRichiesteDaPm');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(2580009, '12345678900', '002434850346951393', '434850346951393', 'e2d633ae0dea4264872d6920102f0f57', 'NOTICE_STORED', '2023-01-31 07:41:02.689', '2023-01-31 07:41:02.961', 2560009, 'activatePaymentNotice', 'sendPaymentOutcomeV2');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(2580004, '12345678900', '002434966942289353', '434966942289353', '9360bcf8d2ad410080241a623f05002d', 'CANCELLED_NORPT', '2023-01-31 07:40:51.206', '2023-01-31 07:42:25.206', 2560004, 'activatePaymentNotice', 'mod3CancelV1');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(2600001, '12345678900', '002953945678852071', '953945678852071', '998adaeca0fa4e0db424d98e5be5b99a', 'FAILED', '2023-01-31 07:41:16.716', '2023-01-31 07:41:17.326', 2580001, 'activatePaymentNotice', 'sendPaymentOutcomeV2');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(2200004, '12345678900', '302117951305429100', '117951305429100', '12345678900302117951305429100-48451', 'CANCELLED', '2023-01-31 07:14:13.755', '2023-01-31 08:02:22.293', 2180004, 'nodoInviaCarrelloRPT', 'annullamentoRptMaiRichiesteDaPm');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(3360014, '12345678900', '302119257159827723', '02119257159827723', '8d0643e872054f0692518e77d677f258', 'CANCELLED', '2023-01-31 08:13:58.606', '2023-01-31 08:31:57.041', 3340014, 'activatePaymentNotice', 'mod3CancelV2');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(3700001, '12345678900', '312116313084213084', '12116313084213084', '0381b30d8178476596df9fbcb9dab8c9', 'FAILED_NORPT', '2023-01-31 08:51:41.510', '2023-01-31 08:51:41.862', 3680001, 'activatePaymentNotice', 'sendPaymentOutcome');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(3340028, '12345678900', '302116463569084691', '02116463569084691', '69f950d6e7484c3084336e79089486fe', 'NOTIFIED', '2023-01-31 08:43:45.006', '2023-01-31 08:43:45.193', 3320028, 'activatePaymentNotice', 'sendPaymentOutcome');
-INSERT INTO position_payment_status_snapshot
+INSERT INTO v_position_payment_status_snapshot
 (id, pa_fiscal_code, notice_id, creditor_reference_id, payment_token, status, inserted_timestamp, updated_timestamp, fk_position_payment, inserted_by, updated_by)
 VALUES(4320008, '12345678900', '302118843241832100', '02118843241832100', 'f5c2b5ee10ce472fa340ad112cca8551', 'CANCELLED', '2023-01-31 09:19:00.651', '2023-01-31 09:22:09.010', 4300008, 'activateIOPayment', 'annullamentoRptMaiRichiesteDaPm');
