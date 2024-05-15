8b33af7c3c91596040e89f81124df289e9c05bad
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-05-15 13:06:59 +0200

    fix schema

--- src/main/java/it/gov/pagopa/nodetsworker/repository/models/PositionPaymentSSEntity.java
+++ src/main/java/it/gov/pagopa/nodetsworker/repository/models/PositionPaymentSSEntity.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 @Data
 @Builder
 @Entity
-@Table(name = "V_POSITION_PAYMENT_STATUS_SNAPSHOT")
+@Table(name = "V_POSITION_PAYMENT_STATUS_SNAPSHOT", schema = "NODO_OFFLINE")
 public class PositionPaymentSSEntity {
 
     @Id
